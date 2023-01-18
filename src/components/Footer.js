cb9292b3f994357571be55550da780e3fb1d520d
Kristopher <kristopher_matic@hotmail.com>
2023-01-18 14:04:46 -0800

    Simple footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FiGithub } from 'react-icons/fi';
+
+const StyledFooter = styled.footer`
+    margin-top: 0 auto;
+
+    div {
+        display: flex;
+        justify-content: center;
+
+        > a {
+            margin: 15px 0 15px 0;
+        }
+    }
+`;
 
 const Footer = () => {
-    return <div></div>;
+    return (
+        <StyledFooter>
+            <div>
+                <a href="https://github.com/kmatic" target="_blank" rel="noopener noreferrer">
+                    kmatic <FiGithub />
+                </a>
+            </div>
+        </StyledFooter>
+    );
 };
 
 export default Footer;
